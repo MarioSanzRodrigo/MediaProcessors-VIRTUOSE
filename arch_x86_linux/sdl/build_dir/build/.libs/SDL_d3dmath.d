6ba2b473ae2837a41a28597f717279db53ba5412
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-05-16 16:56:37 +0200

    .

--- arch_x86_linux/sdl/build_dir/build/.libs/SDL_d3dmath.d
+++
@@ -1,9 +0,0 @@
-build/SDL_d3dmath.lo: \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/render/SDL_d3dmath.c \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/render/../SDL_internal.h \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/render/../dynapi/SDL_dynapi.h \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/render/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_platform.h \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/begin_code.h \
- /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/close_code.h
