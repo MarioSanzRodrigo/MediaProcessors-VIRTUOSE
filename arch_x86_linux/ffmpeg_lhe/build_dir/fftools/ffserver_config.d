c61d685418b9606b54a010d635ae61b937165657
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-05-16 15:51:03 +0200

    fist commit

---
+++ arch_x86_linux/ffmpeg_lhe/build_dir/fftools/ffserver_config.d
@@ -0,0 +1,20 @@
+fftools/ffserver_config.o: src/fftools/ffserver_config.c \
+ src/libavutil/opt.h src/libavutil/rational.h src/libavutil/attributes.h \
+ src/libavutil/avutil.h src/libavutil/common.h src/libavutil/macros.h \
+ src/libavutil/version.h libavutil/avconfig.h src/libavutil/mem.h \
+ src/libavutil/error.h src/libavutil/mathematics.h \
+ src/libavutil/intfloat.h src/libavutil/log.h src/libavutil/pixfmt.h \
+ src/libavutil/dict.h src/libavutil/samplefmt.h \
+ src/libavutil/parseutils.h src/libavutil/avstring.h \
+ src/libavutil/pixdesc.h src/libavutil/avassert.h src/fftools/cmdutils.h \
+ config.h src/libavcodec/avcodec.h src/libavutil/samplefmt.h \
+ src/libavutil/attributes.h src/libavutil/avutil.h src/libavutil/buffer.h \
+ src/libavutil/cpu.h src/libavutil/channel_layout.h src/libavutil/dict.h \
+ src/libavutil/frame.h src/libavutil/buffer.h src/libavutil/log.h \
+ src/libavutil/pixfmt.h src/libavutil/rational.h src/libavcodec/version.h \
+ src/libavutil/version.h src/libavfilter/avfilter.h \
+ src/libavfilter/version.h src/libavformat/avformat.h \
+ src/libavformat/avio.h src/libavutil/common.h src/libavformat/version.h \
+ src/libswscale/swscale.h src/libswscale/version.h \
+ src/fftools/ffserver_config.h src/libavformat/network.h \
+ src/libavutil/error.h src/libavformat/os_support.h src/libavformat/url.h
---
+++ arch_x86_linux/sdl/build_dir/build/.libs/SDL_egl.d
@@ -0,0 +1,36 @@
+build/SDL_egl.lo: \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/SDL_egl.c \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/../SDL_internal.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/../dynapi/SDL_dynapi.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_platform.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/begin_code.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/close_code.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/SDL_sysvideo.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_messagebox.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_stdinc.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_config.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_video.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_pixels.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_endian.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_rect.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_error.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_rwops.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_surface.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_blendmode.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_shape.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_thread.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_atomic.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_platform.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_mutex.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/SDL_vulkan_internal.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_stdinc.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/./khronos/vulkan/vulkan.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/./khronos/vulkan/./vk_platform.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_vulkan.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_log.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/src/video/SDL_egl_c.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_egl.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_loadso.h \
+ /home/mario/Documentos/github/MediaProcessors_selfcontained/3rdplibs/sdl/include/SDL_hints.h
