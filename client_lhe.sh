b7ab9532bdc25c25f959671b204b37458cb59d17
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-05-16 15:37:28 +0200

    remove

--- client_lhe.sh
+++
@@ -1,2 +0,0 @@
-cd /home/mario/Documentos/github/MediaProcessors_selfcontained/gaminganywhere/bin
-LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-client ./config/client.mlhe.abs.conf rtsp://138.4.7.149:8554/desktop
--- server_lhe.sh
+++
@@ -1,2 +0,0 @@
-cd /home/mario/Documentos/github/MediaProcessors_selfcontained/gaminganywhere/bin
-LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-server-periodic ./config/server.mlhe.mame.conf
