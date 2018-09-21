5c6c60c1aed80a855799c3cee7e511fc1622ad6a
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-09-21 15:17:52 +0200

    modify env-setup

--- server_lhe.sh
+++ server_lhe.sh
@@ -1,2 +1,2 @@
-cd /home/mario/Documentos/github/MediaProcessors_selfcontained/gaminganywhere/bin
+cd gaminganywhere/bin
 LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-server-periodic ./config/server.mlhe.mame.conf
